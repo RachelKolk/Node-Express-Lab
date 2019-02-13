48beec8a09ead14230bbafacc42c2a23b6096a4e
RachelKolk <rach.m.obrien@gmail.com>
2019-02-12 19:29:06 -0500

    weird typo fixed

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>LOTL Quote Time!</h1>
+        <h1>LOTR Quote Time!</h1>
         <Posts />
       </div>
     );
